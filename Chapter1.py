8c1ca1e47045f82228c539a38d980bd8aee0258b
emmachoward <46038545+emmachoward@users.noreply.github.com>
2018-12-31 15:14:33 -0600

    Add files via upload

---
+++ Chapter1.py
@@ -0,0 +1,10 @@
+'''
+Created on Aug 24, 2015
+
+@author: Emma Howard, for CPT 135
+'''
+
+name = input("What is your name: ")
+age = int(input("What is your age: "))
+
+print("Well", name, ", I see that you are", age,". Congratulations on making it this far!")
